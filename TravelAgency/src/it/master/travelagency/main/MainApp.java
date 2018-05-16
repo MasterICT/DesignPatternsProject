4f088667e8adf16f15000d0032373fc63d9e5d4c
masterictFS <federico.schinco+mastergit@gmail.com>
2018-05-16 18:42:01 +0200

    gino is going to africa

--- TravelAgency/src/it/master/travelagency/main/MainApp.java
+++ TravelAgency/src/it/master/travelagency/main/MainApp.java
@@ -1,5 +1,10 @@
 package it.master.travelagency.main;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import it.master.travelagency.customer.Customer;
+import it.master.travelagency.customer.Traveler;
 import it.master.travelagency.travel.MultiStep;
 import it.master.travelagency.travel.Step;
 import it.master.travelagency.travel.Travel;
@@ -8,16 +13,27 @@ public class MainApp {
 
 	public static void main(String[] args) throws Exception {
 		
-		Travel africa = new MultiStep("Viaggio in africa");
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 		
-		Travel andata = new Step("Volo andata", 100, "Torino", "Mombasa");
-		africa.addStep(andata);
+		Travel africa = new MultiStep("Africa trip");
 		
-		Travel ritorno = new Step("Volo ritorno", 700, "Mombasa", "Torino");
-		africa.addStep(ritorno);
+		Travel outbound = new Step("Outbound flight", 100, "Torino", "Mombasa");
+		africa.addStep(outbound);
 		
-		System.out.println(africa);
-		System.out.println(africa.getCost());
+		Travel inbound = new Step("Inbound flight", 700, "Mombasa", "Torino");
+		africa.addStep(inbound);
+		
+		Date d = dateFormat.parse("01/01/1960");
+		System.out.println(d);
+		
+		Customer ginoBase = new Customer("Gino", dateFormat.parse("01/01/1960"), "gino@ginomail.com", "Passport", "123");
+		Traveler ginoTraveler = new Traveler(ginoBase, africa);
+		
+		System.out.println(ginoTraveler + " is about to leave for his trip!");
+		System.out.println(ginoTraveler.getTravel());
+		
+		System.out.println(ginoTraveler + " is spending a hefty sum of money: "  +
+				ginoTraveler.getTravel().getCost() + "€!!!");
 	}
 
 }

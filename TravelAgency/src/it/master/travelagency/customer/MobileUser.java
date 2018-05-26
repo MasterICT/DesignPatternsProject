b97971467afd3c8310d2a00db59adf5ef8e4b0a0
masterictFS <federico.schinco+mastergit@gmail.com>
2018-05-26 17:58:12 +0200

    mobile outputs say a bit more

--- TravelAgency/src/it/master/travelagency/customer/MobileUser.java
+++ TravelAgency/src/it/master/travelagency/customer/MobileUser.java
@@ -1,5 +1,6 @@
 package it.master.travelagency.customer;
 
+import it.master.travelagency.Constants;
 import it.master.travelagency.travel.Travel;
 
 public class MobileUser extends CustomerType {
@@ -10,13 +11,24 @@ public class MobileUser extends CustomerType {
 	
 	@Override
 	public void sendNotification(String msgTxt) {
-		System.out.println("sending a push notification");
+		System.out.println("***");
+		System.out.println("Sending a push notification to " + this.getName());
 		System.out.println(msgTxt);
+		System.out.println("***");
 	}
 	
 	public void share(Travel travel, String social) {
+		System.out.println("***");
 		System.out.println("Publishing on: " + social);
 		System.out.println("I am traveling here! " + travel);
+		System.out.println("***");
+	}
+	
+	public void shareMsg(String social, String msg) {
+		System.out.println("***");
+		System.out.println("Sharing on: " + social + " - by: " + this.getName());
+		System.out.println(msg + " #" + Constants.APPNAME);
+		System.out.println("***");
 	}
 
 }

8eec67f4ad23d8c6e679fd723654e5d07b5563d6
masterictFS <federico.schinco+mastergit@gmail.com>
2018-05-26 17:55:17 +0200

    Constants used throughout the app

---
+++ TravelAgency/src/it/master/travelagency/Constants.java
@@ -0,0 +1,12 @@
+package it.master.travelagency;
+
+/**
+ * Constants used throughout the app 
+ *
+ */
+public class Constants {
+	
+	public final static String APPNAME = "MyTravelApp";
+	public final static String POPULAR_SOCIAL_NETWORK = "Twotter";
+	public final static String CREDIT_CARD = "Credit Card";
+}
